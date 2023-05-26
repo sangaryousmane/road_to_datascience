e76cc6050185677e461b7090cb27a0217679f604
ousmane <sangaryousmane@gmail.com>
2023-05-26 09:19:46 +0800

    Add some changes for basic python

--- python-basics/ds-from-scratch/play.py
+++ python-basics/ds-from-scratch/play.py
@@ -22,16 +22,30 @@ def addition(a, b, *args, option=True):
     else:
         return sum;
 
+
 def arg_printer(a, b, option, **kwargs):
     print(f'a = {a}, b = {b}')
     print(f'{option}')
     print(f'{kwargs}')
 
+
 def args_kwargs(a, b, *args, option=True, **kwargs):
     print(f' a = {a}, b = {b}, args = {args}, optin = {option}')
     print(f'kwargs = {kwargs}')
 
-args_kwargs(4, 5, 9, 10, 11, option=True, option1=3, option2=5);
 
+# pack and unpacking
+def args_pack(*args):
+    print(args);
+
+def kwargs_unpack(**kwargs):
+    print(kwargs);
+
+lst = [2, 4, 5]
+dicts = {"age": 23, "name": "Ousmane"}
+
+kwargs_unpack(dicts);
+# args_pack(*lst);
+# args_kwargs(4, 5, 9, 10, 11, option=True, option1=3, option2=5);
 # arg_printer(4, 5, True, param=1, param1=2, param2=3, param3=4);
 # print(f' Sum is: {addition(4, 5, 2, 5, 6, 1, 3, 10, option=True)}');
